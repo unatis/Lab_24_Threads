ac96bf58acdaa3ebdf7cf9086a261170081e958c
Yuri Goncharov <ygpanzer@gmail.com>
2021-06-21 14:29:49 +0200

    Create Monkey.java

---
+++ Monkey.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Monkey extends Thread {
+
+    String MonkeyName;
+    int Age;
+    String MonkeyType;
+
+    public void run() {
+
+        for(int i = 0; i < 100; i++){
+            System.out.println("1111111111");
+        }
+
+    }
+
+}
