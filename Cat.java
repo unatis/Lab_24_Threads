dd6377644ce55e9732cd3a53880092e1e42635de
Yuri Goncharov <ygpanzer@gmail.com>
2021-06-21 14:30:09 +0200

    Create Cat.java

---
+++ Cat.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Cat extends Thread{
+
+    public void run() {
+
+        for(int i = 0; i < 100; i++){
+            System.out.println("2222222222");
+        }
+
+    }
+}
