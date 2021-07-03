57255eb00b0c83abf91e5254e23fbc7f9f659567
Yuri Goncharov <ygpanzer@gmail.com>
2021-07-03 13:18:09 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,10 +5,10 @@ public class Main {
     public static void main(String[] args) {
 
         Monkey monkey = new Monkey();
-        monkey.run();
+        //monkey.run();
 
         Cat cat = new Cat();
-        cat.run();
+        //cat.run();
 
         Thread thread = new Thread(monkey);
         thread.start();
