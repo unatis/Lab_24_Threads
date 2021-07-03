905fd7c8fd8f2477135fe10e1c2c01855276a8f8
Yuri Goncharov <ygpanzer@gmail.com>
2021-07-03 13:18:47 +0200

    Update Monkey.java

--- Monkey.java
+++ Monkey.java
@@ -10,7 +10,7 @@ public class Monkey extends Thread {
     public void run() {
 
         for(int i = 0; i < 100; i++){
-            System.out.println("1111111111");
+            System.out.println("Monkey");
         }
 
     }
